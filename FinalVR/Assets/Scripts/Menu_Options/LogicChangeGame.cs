4269a5bc17598049fbadb3d7b659fb92281a6253
Emanuel-Rdz <j.emanuel.rdz.m@hotmail.com>
2023-05-30 18:49:11 -0600

    Menu de juego con XR, Fade de escena.

--- FinalVR/Assets/Scripts/Menu_Options/LogicChangeGame.cs
+++ FinalVR/Assets/Scripts/Menu_Options/LogicChangeGame.cs
@@ -5,23 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class LogicChangeGame : MonoBehaviour
 {
-	public GameObject gameMenu;
-    private SetGameType optionGame;
-    private int gameSelected = 0;
-
     // Start is called before the first frame update
     void Start(){
-        //optionGame = GameObject.Find("GameMenu").GetComponent<SetGameType>();
-        optionGame = gameMenu.GetComponent<SetGameType>();
-
     }
 
     // Update is called once per frame
     void Update(){
-        gameSelected = optionGame.gameSceneIndex;
+        //
     }
 
-    public void ChangeGame(){
-    	SceneManager.LoadScene(gameSelected);
+    public void ChangeGame(int scene){
+    	int startGame = scene;
+    	SceneManager.LoadScene(startGame);
     }
 }
